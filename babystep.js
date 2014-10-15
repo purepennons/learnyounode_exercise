4f33bc29b13aaeec490064aa394669f852ab9a52
purepennons <y80811@gmail.com>
2014-10-15 15:29:23 +0800

    remove files

--- babystep.js
+++
@@ -1,9 +0,0 @@
-var sum = 0;
-process.argv.forEach(function(val, index, array) {
-	if(index > 1){
-		sum+= parseInt(val, 10);
-	}
-	if(index == process.argv.length - 1)
-		console.log(sum);
-});
-
--- readsync.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-
-var argv = process.argv;
-var filePath = argv[2];
-var array = fs.readFileSync(filePath).toString().split("\n");  //同步讀檔
-console.log(array.length-1);
